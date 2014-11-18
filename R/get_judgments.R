e7936355056a9273a16707a98a66e1a416d7cea2
bchrol <bartek.chrol@gmail.com>
2014-11-18 14:07:13 +0100

    Added function for downloading judgments by IDs

---
+++ R/get_judgments.R
@@ -0,0 +1,33 @@
+#' Get judgments with given IDs
+#' 
+#' Get all judgments, if available, with IDs from a given vector
+#' 
+#' @param idlist integer vector with judgments' IDs
+#' 
+#' @value data.frame with rows corresponding to judgments, or NULL if none
+#'  judgment is available
+#'  
+#'  @examples
+#'  get_judgments(128334)
+#'  get_judgments(c(128334, 77354))
+#'  
+#'  @export
+
+get_judgments <- function(idlist){
+  url <- "https://saos-test.icm.edu.pl/api/judgments/"
+  links <- paste0(url, idlist)
+  result <- lapply(links, function(link){
+    response <- get_response(link)
+    data <- response$data
+    # convert NULLs to NA
+    data <- lapply(data, function(x) if (length(x) == 0){ NA } else { x })
+    data
+  })
+  result <- as.data.frame(do.call(rbind, result))
+  for (i in seq_along(result)){
+    if (all(sapply(result[, i], length) <= 1)){
+      result[, i] <- unlist(result[, i])
+    }
+  }
+  result
+}
