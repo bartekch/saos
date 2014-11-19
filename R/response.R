eccec0a3b451869ac6f24e3108f687830743184c
bchrol <bartek.chrol@gmail.com>
2014-11-19 13:59:58 +0100

    Added function for extracting info about courts from response

--- R/response.R
+++ R/response.R
@@ -12,11 +12,28 @@ get_response <- function(link, simplify = TRUE){
 }
 
 
-# function extractring link for the next page from current response
+# function extracting link for the next page from current response
 extract_link <- function(response){
   links <- response$links
   if (any(links$rel == "next")){
     res <- links[links$rel == "next", "href"]
   } else res <- NULL
   res
+}
+
+
+# function extracting information about judgments in proper structure
+extract_judgments <- function(response){
+  judg <- response$items
+  judg$division <- lapply(1:nrow(judg), function(i) judg$division[i,])
+  jl <- sapply(judg, is.list)
+  
+  # replace NULLs with NAs
+  judg[, jl] <- lapply(judg[, jl], function(col)
+    lapply(col, function(x) if (length(x) == 0) { NA } else { x }))
+  
+  # unlist columns where possible
+  judg[, jl] <- lapply(judg[,jl], function(x)
+    if (all(sapply(x, length) == 1)) { unlist(x) } else { x })
+  judg
 }
