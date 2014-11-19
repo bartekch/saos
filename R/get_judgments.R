6051cff86c46a30a55de4e5f385cb858f36e54df
bchrol <bartek.chrol@gmail.com>
2014-11-19 17:12:27 +0100

    Adjusted structe of files to fulfill ropengov standards

--- R/get_judgments.R
+++ R/get_judgments.R
@@ -32,28 +32,3 @@ get_judgments <- function(idlist){
   }
   result
 }
-
-
-
-#' Get full texts of given judgments
-#' 
-#' Get full texts, if available, of judgments with IDs from a given vector
-#' 
-#' @param idlist integer vector with judgments' IDs
-#' 
-#' @return named character vector with judgments
-#'  
-#' @examples \dontrun{
-#' get_judgment_texts(128334)
-#' get_judgment_texts(c(128334, 77354))
-#'  }
-#'  
-#' @export
-
-get_judgment_texts <- function(idlist){
-  judgments <- get_judgments(idlist)
-  texts <- judgments$textContent
-  # these texts are in HTML, we need pure text as in search/API, but full
-  names(texts) <- idlist
-  texts
-}
