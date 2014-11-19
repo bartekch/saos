bfe419263301f5658b5f02ee1838ae856268d2da
bchrol <bartek.chrol@gmail.com>
2014-11-19 16:56:27 +0100

    Improved description

--- R/saos-package.R
+++ R/saos-package.R
@@ -1,6 +1,18 @@
-#' saos
+#' R interface to the SAOS API
+#' 
+#' Package provides functions to communicate through API with SAOS, a repository
+#'  of judgments from Polish common courts (district, regional and appellate) 
+#'  and the Supreme Court of Poland. 
+#'  
+#' The main function is \code{\link{search_judgments}} for searching for
+#'  judgments matching the given query. 
+#'  
+#' API key is not demanded.
 #'
 #' @name saos-package
 #' @docType package
 #'
+#' @author Bartosz Chroł \email{bartek.chrol@@gmail.com}
+#' 
+#' @keywords package
 NULL
