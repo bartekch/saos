cac846728b9ed8687b0294964d72f1a4d10d8f53
bchrol <bartek.chrol@gmail.com>
2014-11-20 13:45:08 +0100

    Added better search limitting

--- R/search_judgments.R
+++ R/search_judgments.R
@@ -5,6 +5,7 @@
 #' @param query Search pattern as a named list; pairs name-value are treated
 #'   as query parameters with corresponding values, see Details.
 #' @param limit Limit the number of search results.
+#' @param force If TRUE, force search request even if it seems extreme.
 #' 
 #' @details The available query parameters:
 #' 
@@ -17,7 +18,17 @@
 #'  
 #' @export
 
-search_judgments <- function(query = NULL, limit = 100){
+search_judgments <- function(query = NULL, limit = 200, force = FALSE){
+  
+  count <- count_judgments(query)
+  if (limit > count) limit <- count
+    
+  if ((limit > 200) & !force){
+    limit <- 200
+    warning(sprintf("Pulling down only 200 out of expected %s results. If you
+are sure to pull down everything use force = TRUE", limit))
+  }
+  
   query <- paste_query(query)
   url <- "https://saos-test.icm.edu.pl/api/search/judgments/?pageSize=100&"
   link <- paste0(url, query)
@@ -28,7 +39,7 @@ search_judgments <- function(query = NULL, limit = 100){
   while (!is.null(next_page) & (number < limit)){
     response <- get_response(next_page)
     judgments <- rbind(judgments, extract_judgments(response))
-    number <- number + nrow(judgments)
+    number <- nrow(judgments)
     next_page <- extract_link(response)
   }
   if (number > limit){
