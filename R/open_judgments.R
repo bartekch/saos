cccfece1be2df1b2f788576f504306bd429b0770
bchrol <bartek.chrol@gmail.com>
2015-02-10 14:49:26 +0100

    Updating open_judgments function

--- R/open_judgments.R
+++ R/open_judgments.R
@@ -2,25 +2,38 @@
 #' 
 #' Open in web browser SAOS pages for given judgments.
 #' 
-#' @param judgments Data frame of search results, as returned from 
-#'   \code{\link{search_judgments}} or \code{\link{get_judgments}}.
-#' @param limit Limit the number of opened pages.
+#' @param x An object used to select a method.
+#' @param limit Integer. Limit the number of opened pages.
 #' 
 #' @return (Invisibly) Vector of character strings with URLs of abstracts opened.
 #'  
 #' @examples \donttest{
-#' judgments <- search_judgments()
+#' 
+#' open_judgments(1:3)
+#' 
+#' judgments <- search_judgments(limit = 5, verbose = FALSE)
 #' open_judgments(judgments)
 #'  }
 #'  
 #' @export
 #' 
-open_judgments <- function(judgments, limit = 5){
+
+open_judgments <- function(x, limit = 5) UseMethod("open_judgments")
+
+# default method
+#' @export
+open_judgments.default <- function(x){
+  stop("open_judgments accept arguments of class 'saos_search' or numeric vectors.")
+}
+
+
+#' @describeIn open_judgments Open in web browser SAOS pages for judgments with
+#'   given IDs.
+#' @export
+open_judgments.numeric <- function(x, limit = 5){
   stopifnot(limit >= 1)
-  links <- paste("https://saos-test.icm.edu.pl/judgments/", judgments$id, 
-                 sep = "")
-  links <- links[!is.na(judgments$courtCases)]
-  
+  links <- paste("https://saos-test.icm.edu.pl/judgments/", x, sep = "")
+    
   if (length(links) > limit) {
     warning("More pages (", length(links), ") than maximum to be opened (", 
             limit, ").")
@@ -33,3 +46,24 @@ open_judgments <- function(judgments, limit = 5){
   invisible(links)
 }
 
+
+
+#' @describeIn open_judgments Open in web browser SAOS pages for judgments with
+#'   from the given search results.
+#' @export
+open_judgments.saos_search <- function(x, limit = 5){
+  stopifnot(limit >= 1)
+  links <- sapply(x, `[[`, "href")
+  links <- gsub("api/", "", links)
+  
+  if (length(links) > limit) {
+    warning("More pages (", length(links), ") than maximum to be opened (", 
+            limit, ").")
+    links <- links[1:limit]
+  }
+  
+  for (link in links) {
+    utils::browseURL(link)
+  }
+  invisible(links)
+}
