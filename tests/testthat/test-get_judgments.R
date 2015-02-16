1f62b3224ffe55950dfa92c4443170bfe6071241
bchrol <bartek.chrol@gmail.com>
2015-02-16 15:58:14 +0100

    Small fix in tests to avoid unneccesary printing

--- tests/testthat/test-get_judgments.R
+++ tests/testthat/test-get_judgments.R
@@ -4,6 +4,7 @@ test_that("get with no results returns proper object", {
   empty <- list()
   class(empty) <- c("saos_judgments", "list")
   
-  expect_identical(get_judgments(1234567890, verbose = FALSE), empty)
+  expect_identical(suppressMessages(get_judgments(1234567890, verbose = FALSE)),
+                   empty)
   expect_identical(get_judgments(empty_search_result(), verbose = FALSE), empty) 
 })
